89e7f42f71012b7a31824d5bdeeb3529187bc732
dyharlan <dyharlan45@gmail.com>
2024-01-18 22:28:42 +0800

    Use a different stub for IApplication class

--- app/src/main/java/com/nttdocomo/ui/IApplication.java
+++ app/src/main/java/com/nttdocomo/ui/IApplication.java
@@ -1,114 +1,104 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
+/*
+ * @(#)IApplication.java	1.15 02/09/19
+ *
+ * Copyright 2001 NTT DoCoMo, Inc. All rights reserved.
+ */
 
 package com.nttdocomo.ui;
 
-//import com.nttdocomo.util.ScheduleDate;
+//import com.nttdocomo.lang.*;
 
-public abstract class IApplication {
-    public static final int LAUNCHED_FROM_MENU = 0;
-    public static final int LAUNCHED_AFTER_DOWNLOAD = 1;
-    public static final int LAUNCHED_FROM_TIMER = 2;
-    public static final int LAUNCHED_AS_CONCIERGE = 3;
-    public static final int LAUNCHED_FROM_EXT = 4;
-    public static final int LAUNCHED_FROM_BROWSER = 5;
-    public static final int LAUNCHED_FROM_MAILER = 6;
-    public static final int LAUNCHED_FROM_IAPPLI = 7;
-    public static final int LAUNCHED_FROM_LAUNCHER = 8;
-    public static final int LAUNCHED_AS_ILET = 9;
-    public static final int LAUNCHED_MSG_RECEIVED = 10;
-    public static final int LAUNCHED_MSG_SENT = 11;
-    public static final int LAUNCHED_MSG_UNSENT = 12;
-    public static final int LAUNCHED_FROM_LOCATION_INFO = 13;
-    public static final int LAUNCHED_FROM_LOCATION_IMAGE = 14;
-    public static final int LAUNCHED_FROM_PHONEBOOK = 15;
-    public static final int LAUNCHED_FROM_DTV = 17;
-    public static final int LAUNCHED_FROM_TORUCA = 18;
-    public static final int LAUNCHED_FROM_FELICA_ADHOC = 19;
-    public static final int LAUNCHED_FROM_MENU_FOR_DELETION = 20;
-    public static final int LAUNCHED_FROM_BML = 21;
-    public static final int LAUNCH_BROWSER = 1;
-    public static final int LAUNCH_VERSIONUP = 2;
-    public static final int LAUNCH_IAPPLI = 3;
-    public static final int LAUNCH_AS_LAUNCHER = 4;
-    public static final int LAUNCH_MAILMENU = 5;
-    public static final int LAUNCH_SCHEDULER = 6;
-    public static final int LAUNCH_MAIL_RECEIVED = 7;
-    public static final int LAUNCH_MAIL_SENT = 8;
-    public static final int LAUNCH_MAIL_UNSENT = 9;
-    public static final int LAUNCH_MAIL_LAST_INCOMING = 10;
-    public static final int LAUNCH_DTV = 12;
-    public static final int LAUNCH_BROWSER_SUSPEND = 13;
-    public static final int SUSPEND_BY_NATIVE = 1;
-    public static final int SUSPEND_BY_IAPP = 2;
-    public static final int SUSPEND_PACKETIN = 256;
-    public static final int SUSPEND_CALL_OUT = 512;
-    public static final int SUSPEND_CALL_IN = 1024;
-    public static final int SUSPEND_MAIL_SEND = 2048;
-    public static final int SUSPEND_MAIL_RECEIVE = 4096;
-    public static final int SUSPEND_MESSAGE_RECEIVE = 8192;
-    public static final int SUSPEND_SCHEDULE_NOTIFY = 16384;
-    public static final int SUSPEND_MULTITASK_APPLICATION = 32768;
+import javax.microedition.midlet.MIDlet;
 
-    public static final IApplication getCurrentApp() {
-        return null;
-    }
+/**
+ * This provides the template of an application.
+ * Any application must be created inheriting this class.
+ * <BR>The resume method is the method called right after an application returns from suspend status.  In case a current frame exists, a resume event is thrown to the frame and then the resume method is called.  Application programmers must be aware of this order when coding.
+ * <P>When creating an application class by using this class, note the following.
+ * <BR>Application programmers must not directly generate an instance of the IApplication's subclass.
+ * <BR>When explicitly defining a constructor of a subclass of this class, designate "public" for the access identifier.  Also note that a constructor having parameters cannot be used to create an application object.  When the constructor is not defined, a default constructor will be generated.
+ * <PRE>
+ * Example 1: When not describing constructor explicitly
+ *
+ *  public MyApp extends IApplication {
+ *    public void start() {
+ *      ...
+ *    }
+ *  }
+ *
+ *
+ * Example 2: When describing constructor explicitly
+ *
+ *   public MyApp2 extends IApplication {
+ *      public MyApp2() {
+ *        ...
+ *      }
+ *      public void start() {
+ *      ...
+ *      }
+ *  }
+ * </PRE>
+ */
 
+public abstract class IApplication extends MIDlet {
+
+    /**
+     * An application cannot directly generate an instance of this class.
+     */
     public IApplication() {
     }
 
-    public final String[] getArgs() {
-        return null;
-    }
+    /**
+     * This obtains the launch parameter of an application.
+     * This method can obtain the parameter designated by ADF.
+     *
+     * @return The alignment of character string of the launch parameter of an application.  When the parameter is not designated by ADF, the alignment of 0-length will be returned.
+     */
+//    public final String[] getArgs(){
+//    }
 
-    public final String getParameter(String var1) {
-        return null;
-    }
+    /**
+     * This obtains the URL from which the application was downloaded.
+     * For example, when the JAR-form file in which an application is stored is loaded as http://www.foo.com./download/prog/App1.jar, the return value is http://foo.com/download/prog.
+     *
+     * @return The character string indicating the URL from which an application was downloaded.
+     */
+    //public native String getSourceURL();
 
+    /**
+     * This obtains the object of the current application.
+     *
+     * @return The current application object.
+     */
+//    public static final IApplication getCurrentApp() {
+//    }
+
+    /**
+     * Method to be called after the launch of application
+     */
     public abstract void start();
 
-    public void resume() {
-    }
+    /**
+     * Method to be called right after the application resumes from the suspended status.  When resume processing is necessary, this method is overridden by the class from which IApplication was inherited.
+     */
+    public void resume() { }
 
+    /**
+     * Only method to close the application.  When System.exit() is called in the scope of IApplication, SecurityException generated.
+     */
     public final void terminate() {
     }
-
-    public final String getSourceURL() {
-        return null;
+    //wrappers for iappli functions
+    @Override
+    public void destroyApp(boolean unconditional){
+        terminate();
     }
+    @Override
+    public void pauseApp(){
 
-    public final int getLaunchType() {
-        return 0;
     }
-
-    public final void launch(int var1, String[] var2) {
-    }
-
-    public int getSuspendInfo() {
-        return 0;
-    }
-
-//    public void setLaunchTime(int var1, ScheduleDate var2) {
-//    }
-//
-//    public ScheduleDate getLaunchTime(int var1) {
-//        return null;
-//    }
-//
-//    public PushManager getPushManager() throws SecurityException {
-//        return null;
-//    }
-
-    public final boolean isMoved() {
-        return false;
-    }
-
-    public final void clearMoved() {
-    }
-
-    public final boolean isMovedFromOtherTerminal() {
-        return false;
+    @Override
+    public void startApp(){
+        start();
     }
 }
